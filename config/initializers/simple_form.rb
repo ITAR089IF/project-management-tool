dffc9dea992ccb7dc6317c0768591974cda1f2c3
Kasvit <kasvit93@gmail.com>
2018-10-24 05:06:21 -0500

    fix checkbox and form

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -22,7 +22,7 @@ SimpleForm.setup do |config|
   config.wrappers :inline_checkbox, tag: 'div', class: 'control-group', error_class: 'error' do |b|
     b.use :html5
     b.wrapper tag: 'div', class: 'controls' do |ba|
-      ba.use :label_input, wrap_with: { class: 'checkbox inline' }
+      ba.use :label_input, wrap_with: { class: 'checkbox inline self_checkbox' }
       ba.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
       ba.use :hint, wrap_with: { tag: 'p', class: 'help-block' }
     end
