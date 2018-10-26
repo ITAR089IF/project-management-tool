dc146b1c2aa5b33406e97d7c84f7fc6ad1f6ae0f
supersem <xsupersemx@gmail.com>
2018-10-26 11:57:30 +0300

    fix conflicts + update test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -27,7 +27,7 @@ FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    sequence(:email) { |index| Faker::Internet.email.sub(/\@/, "#{index}_@") }
+    sequence(:email) { |index| Faker::Internet.email.sub(/\@/, "_#{index}@") }
     password { Faker::Internet.password }
   end
 end
