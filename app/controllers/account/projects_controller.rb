62591a59262e686a993017c586b950d4eaab20d6
Natalia <nataliaboyko11081997@gmail.com>
2018-10-27 20:13:56 +0300

    fix controller show

--- app/controllers/account/projects_controller.rb
+++ app/controllers/account/projects_controller.rb
@@ -7,8 +7,8 @@ class Account::ProjectsController < Account::AccountController
   def show
     @workspace = parent
     @project = @workspace.projects.find(params[:id])
-    @incomplete_tasks = @project.tasks.incomplete
-    @complete_tasks = @project.tasks.complete
+    @incomplete_tasks = @project.tasks.incomplete.row_order_asc
+    @complete_tasks = @project.tasks.complete.row_order_asc
   end
 
   def new
