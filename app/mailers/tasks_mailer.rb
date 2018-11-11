83160d6e0e4189cdc8924fbc5bb119b3e5fc995b
Intrast <kovach.bohdan@ukr.net>
2018-11-12 01:58:14 +0200

    fix

--- app/mailers/tasks_mailer.rb
+++ app/mailers/tasks_mailer.rb
@@ -3,7 +3,7 @@ class TasksMailer < ApplicationMailer
 
   def task_assign_to_user_email(task)
     @task = task
-    @user = User.find_by(id: task.assignee_id)
+    @user = @task.assignee
     mail(to: @user.email, subject: 'Task detail')
   end
 end
