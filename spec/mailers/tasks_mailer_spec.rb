7dba2bc2ee0157a8ee50d3915747ea39e815056f
Natalia <nataliaboyko11081997@gmail.com>
2018-11-12 23:43:06 +0200

    fix test

--- spec/mailers/tasks_mailer_spec.rb
+++ spec/mailers/tasks_mailer_spec.rb
@@ -2,32 +2,54 @@ require "rails_helper"
 include ActiveJob::TestHelper
 
 RSpec.describe TasksMailer, type: :mailer do
+
+  let!(:user) { create(:user) }
+  let!(:current_user) { user }
+  let!(:user1) { create(:user) }
+  let!(:workspace) { create(:workspace, user: user) }
+  let!(:project) { create(:project, workspace: workspace, users: [user]) }
+  let!(:task1) { create(:task, project: project) }
+  let!(:task) { create(:task, watchers: [user1, user]) }
+
   describe "task_completed" do
 
-    let!(:user) { create(:user) }
-    let!(:current_user) { user }
-    let!(:user1) { create(:user) }
-    let!(:workspace) { create(:workspace, user: user) }
-    let!(:project) { create(:project, workspace: workspace, users: [user]) }
-    let!(:task1) { create(:task, project: project) }
-    let!(:task) { create(:task, watchers: [user1]) }
-    let!(:mail) { TasksMailer.task_completed(@task, current_user).deliver_later }
+    it 'job is created' do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        TasksMailer.task_completed(task, current_user).deliver_later
+      }.to have_enqueued_job.on_queue('mailers')
+    end
 
-    it 'renders the subject' do
+    it 'welcome_email is sent' do
+      expect {
+        perform_enqueued_jobs do
+          TasksMailer.task_completed(task, current_user).deliver_later
+        end
+      }.to change { ActionMailer::Base.deliveries.size }.by(1)
+    end
+
+    it 'email is sent to the right user' do
+      perform_enqueued_jobs do
+        TasksMailer.task_completed(task, current_user).deliver_later
+      end
+      mail = ActionMailer::Base.deliveries.last
+      expect(mail.to[0]).to eq user1.email
+    end
+
+    it 'email subject' do
+      perform_enqueued_jobs do
+        TasksMailer.task_completed(task, current_user).deliver_later
+      end
+      mail = ActionMailer::Base.deliveries.last
       expect(mail.subject).to eql("Task completed")
     end
 
-    it 'renders the sender email' do
-      expect(mail.from).to eql('info@asana.com')
-    end
-
-    it 'renders the receiver email' do
-      expect(mail.to).to eql(task.watchers.first.email)
-    end
-
-    it 'assigns @title' do
+    it 'email title' do
+      perform_enqueued_jobs do
+        TasksMailer.task_completed(task, current_user).deliver_later
+      end
+      mail = ActionMailer::Base.deliveries.last
       expect(mail.body.encoded).to include(task.title)
     end
-
   end
 end
