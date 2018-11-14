e8c73d40cea3aba24ee6262d35eaf2c0c7dc9fbc
Kasvit <kasvit93@gmail.com>
2018-11-14 08:16:43 -0600

    fix wrapper

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -38,7 +38,7 @@ SimpleForm.setup do |config|
     b.use :error, wrap_with: { tag: :div, class: "help is-danger" }
   end
 
-  config.wrappers :upload_avatar do |b|
+  config.wrappers :upload_file do |b|
     b.use :html5
     b.use :placeholder
     b.use :label, class: "button is-small"
