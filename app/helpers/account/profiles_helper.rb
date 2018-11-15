faba73e9e93c1e21c27a5cc15b9620386bd1efb6
Kasvit <kasvit93@gmail.com>
2018-11-15 08:21:03 -0600

    fix view #show

--- app/helpers/account/profiles_helper.rb
+++ app/helpers/account/profiles_helper.rb
@@ -3,11 +3,4 @@ module Account::ProfilesHelper
     image_tag((user.with_avatar? ? url_for(user.avatar) : "128x128.png"),  class: 'is-rounded')
   end
 
-  def profile_initials(user)
-    if user.with_avatar?
-      image_tag(user.avatar)
-    else
-      user.user_initials
-    end
-  end
 end
