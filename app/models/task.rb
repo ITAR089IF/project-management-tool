c2e9b17ab8e3b659147e59376a2dafe8283314e1
Natalia <nataliaboyko11081997@gmail.com>
2018-11-16 02:51:56 +0200

    fix

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,7 +8,7 @@
 #  description :text
 #  due_date    :datetime
 #  row_order   :integer
-#  section     :boolean          default(FALSE)
+#  section     :boolean
 #  title       :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
