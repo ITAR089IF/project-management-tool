962ade254eb43b96a33fc844af188e5a4cb35538
supersem <xsupersemx@gmail.com>
2018-11-20 11:51:57 +0200

    update replace up/down with drug&drop

--- app/assets/javascripts/sort.js
+++ app/assets/javascripts/sort.js
@@ -1,4 +1,4 @@
- // Sortable.create(incomplete_tasks, {
- //   handle: '.glyphicon-move',
- //   animation: 150
- // })
+$(document).ready(function() {
+  Sortable.create(incomplete_tasks, { handle: '.glyphicon-move', animation: 150 });
+  Sortable.create(complete_tasks, { handle: '.glyphicon-move', animation: 150 });
+});
