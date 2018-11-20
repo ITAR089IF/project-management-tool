37933929377645d632f3e13405ee5b1658cce1fd
ShevchukTania <taniabeley@ukr.net>
2018-11-20 10:39:14 +0200

    close modal improvemant

--- app/assets/javascripts/account/workspaces.js
+++ app/assets/javascripts/account/workspaces.js
@@ -2,4 +2,9 @@ $( document ).on('turbolinks:load', function() {
   $('.modal-background').click(function(){
     $('.modal').removeClass('is-active');
   });
+  $('.close').click(function(e){
+    $('.modal').removeClass('is-active');
+    $(e).parent().find('modal-card-title').html('');
+    $(e).parent().parent().find('modal-card-body').html('');
+  });
 });
