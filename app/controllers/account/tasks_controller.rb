ba1b655c60b46ed4c956c3358da394cd65674b35
ShevchukTania <taniabeley@ukr.net>
2018-11-30 20:55:20 +0200

    removed unusefull js

--- app/controllers/account/tasks_controller.rb
+++ app/controllers/account/tasks_controller.rb
@@ -140,7 +140,7 @@ class Account::TasksController < Account::AccountController
   end
 
   def create_task_from_calendar
-    @project = current_user.available_projects.where(id: calendar_task_params[:project_id])[0]
+    @project = current_user.available_projects.where(id: calendar_task_params[:project_id]).first
     if @project.present?
       @task = @project.tasks.build(calendar_task_params)
       if @task.save
