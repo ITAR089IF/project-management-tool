772d380ed868bb774c3e3c0e6bd0708d66928d3a
Bogdan Busko <busko.bogdan@gmail.com>
2018-12-05 13:55:58 +0200

    fix search

--- spec/models/workspace_spec.rb
+++ spec/models/workspace_spec.rb
@@ -51,9 +51,9 @@ RSpec.describe Workspace, type: :model do
     end
 
     it 'should find workspaces with entered text' do
-      expect(user.workspaces.search_workspaces('iT inn').count).to eq 1
-      expect(user.workspaces.search_workspaces('inn').count).to eq 2
-      expect(user.workspaces.search_workspaces('hasgd').count).to eq 0
+      expect(user1.available_workspaces.search_workspaces('iT inn').count).to eq 1
+      expect(user.available_workspaces.search_workspaces('inn').count).to eq 2
+      expect(user.available_workspaces.search_workspaces('hasgd').count).to eq 0
     end
   end
 
