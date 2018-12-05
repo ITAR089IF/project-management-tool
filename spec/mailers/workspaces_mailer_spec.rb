57a129ed144d4452887ccfb5de288506a247dc16
Kasvit <kasvit93@gmail.com>
2018-12-05 15:04:31 -0600

    add flash

--- spec/mailers/workspaces_mailer_spec.rb
+++ spec/mailers/workspaces_mailer_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe WorkspacesMailer, type: :mailer do
   let!(:user) { create(:user) }
   let!(:workspace) { create(:workspace, user: user) }
   let!(:file) { WickedPdf.new.pdf_from_string('<h1>Hello There!</h1>') }
+  let!(:mail) { WorkspacesMailer.workspace_details(workspace, user, file) }
   
   describe "workspace mailer" do
 
@@ -12,19 +13,7 @@ RSpec.describe WorkspacesMailer, type: :mailer do
       expect {WorkspacesMailer.workspace_details(workspace, user, file).deliver_later}.to have_enqueued_job.on_queue('mailers')
     end
 
-    it 'email is sent' do
-      expect {
-        perform_enqueued_jobs do
-          WorkspacesMailer.workspace_details(workspace, user, file).deliver_later
-        end
-      }.to change { ActionMailer::Base.deliveries.size }.by(1)
-    end
-
     it 'email subject' do
-      perform_enqueued_jobs do
-        WorkspacesMailer.workspace_details(workspace, user, file).deliver_later
-      end
-      mail = ActionMailer::Base.deliveries.last
       expect(mail.subject).to eq("You have been received workspace details")
     end
 
