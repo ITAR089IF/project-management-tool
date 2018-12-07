0755604e2b095e09a16408f9e16f42c8d7108b4f
Bogdan Busko <busko.bogdan@gmail.com>
2018-12-07 17:11:39 +0200

    rework

--- app/javascript/bundles/Dashboard/components/Dashboard.jsx
+++ app/javascript/bundles/Dashboard/components/Dashboard.jsx
@@ -6,58 +6,50 @@ import RGL, { WidthProvider } from "react-grid-layout";
 const ReactGridLayout = WidthProvider(RGL);
 
 export class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
-    const layout = this.generateLayout();
+    const layout = [
+      {i: 'a', x: 0, y: 0, w: 1, h: 2},
+      {i: 'b', x: 1, y: 0, w: 3, h: 2},
+      {i: 'c', x: 4, y: 0, w: 1, h: 2}
+    ];
+
     this.state = { layout };
+
+    this.onLayoutChange = this.onLayoutChange.bind(this);
   }
 
   generateDOM() {
-    return _.map(_.range(this.props.items), (i) => {
-      return (
-        <div className="box" key={i}>
-          <span className="text">{i}</span>
-        </div>
+    return(_.map(this.state.layout, (l, i) => {
+      return(
+        <div className='box' key={l.i} data-grid={l}>{l.i}</div>
       );
-    });
-  }
-
-  generateLayout() {
-    const p = this.props;
-    return _.map(new Array(p.items), (item, i) => {
-      const y = _.result(p, "y") || Math.ceil(Math.random() * 4) + 1;
-      return {
-        x: (i * 2) % 12,
-        y: Math.floor(i / 6) * y,
-        w: 2,
-        h: y,
-        i: i.toString()
-      };
-    });
+    }));
   }
 
   onLayoutChange(layout) {
+    this.setState({ layout });
     this.props.onLayoutChange(layout);
   }
 
   render() {
     return (
       <ReactGridLayout
+        {...this.props}
         layout={this.state.layout}
         onLayoutChange={this.onLayoutChange}
-        {...this.props}
       >
         {this.generateDOM()}
       </ReactGridLayout>
-    );
+    )
   }
 }
 
 Dashboard.defaultProps = {
   className: "layout",
-  items: 20,
+  cols: 12,
   rowHeight: 30,
-  onLayoutChange: () => {},
-  cols: 12
+  width: 1200,
+  onLayoutChange: function() {}
 }
