85f1c3529c908d673cea7b9359fcddb710358085
Bogdan Busko <busko.bogdan@gmail.com>
2018-12-10 13:18:23 +0200

    fix code

--- app/javascript/bundles/Dashboard/components/Dashboard/Dashboard.jsx
+++ app/javascript/bundles/Dashboard/components/Dashboard/Dashboard.jsx
@@ -6,7 +6,8 @@ import { WidthProvider, Responsive } from "react-grid-layout";
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 import "./dashboard.scss";
-
+import "../../../../../../node_modules/react-grid-layout/css/styles.css";
+import "../../../../../../node_modules/react-resizable/css/styles.css";
 
 class Dashboard extends React.Component {
   constructor(props) {
@@ -44,4 +45,4 @@ class Dashboard extends React.Component {
   }
 }
 
-export { Dashboard };
+export default Dashboard;
