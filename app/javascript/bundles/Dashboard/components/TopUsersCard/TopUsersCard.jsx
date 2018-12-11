c8816aecc2fe0aef7eac72d197ce375d677fa894
Natalia <nataliaboyko11081997@gmail.com>
2018-12-11 22:52:57 +0200

    fix

--- app/javascript/bundles/Dashboard/components/TopUsersCard/TopUsersCard.jsx
+++ app/javascript/bundles/Dashboard/components/TopUsersCard/TopUsersCard.jsx
@@ -5,11 +5,11 @@ import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, LabelList, Legend} 
 import "./top-users-card.scss";
 
 const data = [
-      {name: 'user 1', completed_tasks: 25},
-      {name: 'user 2', completed_tasks: 30},
-      {name: 'user 3', completed_tasks: 24},
-      {name: 'user 4', completed_tasks: 15},
-      {name: 'user 5', completed_tasks: 3},
+      {name: 'user', completed: 25},
+      {name: 'user 2', completed: 30},
+      {name: 'user 3', completed: 24},
+      {name: 'user 4', completed: 15},
+      {name: 'user 5', completed: 3},
 ];
 
 const renderCustomizedLabel = (props) => {
@@ -20,7 +20,7 @@ const renderCustomizedLabel = (props) => {
     <g>
       <circle cx={x + width / 2} cy={y - radius} r={radius} fill="#8884d8" />
       <text x={x + width / 2} y={y - radius} fill="#fff" textAnchor="middle" dominantBaseline="middle">
-        {value.split(' ')[1]}
+        {value.split(' ')[0]}
       </text>
     </g>
   );
@@ -37,13 +37,13 @@ class SimpleBarChart extends React.Component {
   render () {
   	return (
     	<BarChart width={600} height={300} data={data}
-            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-       <CartesianGrid strokeDasharray="3 3"/>
+            margin={{top: 5, right: 30, left: 40, bottom: 5}}>
+       <CartesianGrid strokeDasharray="10 10"/>
        <XAxis dataKey="name"/>
        <YAxis/>
        <Tooltip/>
        <Legend />
-       <Bar dataKey="completed_tasks" fill="#8884d8" minPointSize={5}>
+       <Bar dataKey="completed" fill="#8884d8" minPointSize={5}>
           <LabelList dataKey="name" content={renderCustomizedLabel} />
         </Bar>
       </BarChart>
