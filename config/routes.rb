b61b51c568f530c9ad0a88a644fe70b2ed61cd15
Oleksandr <admin@intema.com.ua>
2018-12-11 12:56:59 +0200

    Update config/routes.rb
    Co-Authored-By: ShevchukTania <taniabeley@ukr.net>

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :account do
     get '/dashboard', to: 'dashboard#index'
-    get '/tasks_info_card', to: 'dashboard#tasks_info_card'
+    get '/tasks-info-card', to: 'dashboard#tasks_info_card'
     get '/calendar', to: 'dashboard#calendar'
     get '/inbox', to: 'dashboard#inbox'
     get '/reports/workspaces/:workspace_id', to: 'reports#workspace', as: :workspace_report
