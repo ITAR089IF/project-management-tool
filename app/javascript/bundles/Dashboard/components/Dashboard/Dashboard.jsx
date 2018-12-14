42658fc78df85ba2ca7d39b0837b636cd53c4c2c
ShevchukTania <taniabeley@ukr.net>
2018-12-14 13:46:35 +0200

    import Component to dashboard

--- app/javascript/bundles/Dashboard/components/Dashboard/Dashboard.jsx
+++ app/javascript/bundles/Dashboard/components/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import _ from "lodash";
 import { WidthProvider, Responsive } from "react-grid-layout";
+import { TasksInfoCard } from '../TasksInfoCard'
 
 import * as api from '../../Api/layout_api';
 import * as config from '../config.js';
@@ -43,7 +44,7 @@ class Dashboard extends React.Component {
         onLayoutChange={(layout) => this.onLayoutChange(layout)}
         layouts={{lg: this.state.layout}}
       >
-        <div className="box" key="a" data-grid={{i: 'a', x: 0, y: 0, w: 1, h: 2}}>a</div>
+        <TasksInfoCard key="a" data-grid={{i: 'a', x: 0, y: 0, w: 1, h: 2}}></TasksInfoCard>
         <div className="box" key="b" data-grid={{i: 'b', x: 1, y: 0, w: 1, h: 2}}>b</div>
         <div className="box" key="c" data-grid={{i: 'c', x: 2, y: 0, w: 1, h: 2}}>c</div>
         <div className="box" key="d" data-grid={{i: 'd', x: 3, y: 0, w: 1, h: 2}}>d</div>
