6db6a96548e80b427e92e7321411340b59d041d7
ShevchukTania <taniabeley@ukr.net>
2018-12-15 16:00:18 +0200

    WIP

--- app/javascript/bundles/Dashboard/components/Dashboard/Dashboard.jsx
+++ app/javascript/bundles/Dashboard/components/Dashboard/Dashboard.jsx
@@ -44,13 +44,10 @@ class Dashboard extends React.Component {
         onLayoutChange={(layout) => this.onLayoutChange(layout)}
         layouts={{lg: this.state.layout}}
       >
-        <div className="box" key="a" data-grid={{i: 'a', x: 0, y: 0, w: 1, h: 2}}>
-          <TasksInfoCard width={this.props.width/4}/>
+        <div className="box" key="a" width={this.props.width} data-grid={{i: 'a', x: 0, y: 0, w: 2, h: 2}}>
+          <TasksInfoCard width={this.props.width}/>
         </div>
-        <div className="box" key="b" data-grid={{i: 'b', x: 1, y: 0, w: 2, h: 2}}>b</div>
-        <div className="box" key="c" data-grid={{i: 'c', x: 2, y: 0, w: 1, h: 2}}>c</div>
-        <div className="box" key="d" data-grid={{i: 'd', x: 3, y: 0, w: 1, h: 2}}>d</div>
-        <div className="box" key="e" data-grid={{i: 'e', x: 4, y: 0, w: 1, h: 2}}>e</div>
+
       </ResponsiveReactGridLayout>
     )
   }
--- app/javascript/bundles/Dashboard/components/TasksInfoCard/TasksInfoCard.jsx
+++ app/javascript/bundles/Dashboard/components/TasksInfoCard/TasksInfoCard.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import _ from "lodash";
-import { ComposedChart, Line, Area, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import "./tasks-info-card.scss";
+import SelectWorkspace from "../SelectWorkspace";
+import SameDataComposedChart from "../SameDataComposedChart";
 
 class TasksInfoCard extends React.Component {
   constructor(props) {
@@ -42,58 +43,13 @@ class TasksInfoCard extends React.Component {
     return (
       <div>
         <SelectWorkspace workspaces={this.state.workspaces} active={this.state.active} onClick={(id) => this.handleClick(id)}/>
-        <SameDataComposedChart data={this.state.data} width={this.props.width}/>
+        <SameDataComposedChart data={this.state.data} width={300}/>
       </div>
     )
   }
 }
 
-class SameDataComposedChart extends React.Component {
-	render () {
-    const width = this.props.width;
-    console.log(width);
-  	return (
-      <ComposedChart width={width} height={2*width/3} data={this.props.data} >
-        <CartesianGrid stroke='#777'/>
-        <XAxis dataKey="name" fontSize='0.7rem'/>
-        <YAxis fontSize='0.7rem'/>
-        <Tooltip/>
-        <Legend width={width} wrapperStyle={{fontSize: '0.7rem', left: '0'}}/>
-        <Bar dataKey='uncompleted' barSize={width/30} fill='#413ea0'/>
-        <Line type='monotone' dataKey='due soon' stroke='#ff7300'/>
-        <Line type='monotone' dataKey='outdated' stroke='#ff4306'/>
-      </ComposedChart>
-    );
-  }
-}
 
-class SelectWorkspace extends React.Component {
 
-	render () {
-    const workspaces = this.props.workspaces;
-  	return (
-      <div className="dropdown is-hoverable">
-        <div className="dropdown-trigger">
-          <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
-            <span>{this.props.active}</span>
-            <span className="icon is-small">
-              <i className="fas fa-angle-down" aria-hidden="true"></i>
-            </span>
-          </button>
-        </div>
-        <div className="dropdown-menu" id="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <li className="dropdown-item" key='all' onClick={() => this.props.onClick('all')}>All Workspaces</li>
-            { Object.entries(workspaces).map(([key, value]) => {
-              return <li href="#" className="dropdown-item" key={key} onClick={() => this.props.onClick(key)}>
-                        {value}
-                      </li>
-            })}
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
 
 export default TasksInfoCard;
