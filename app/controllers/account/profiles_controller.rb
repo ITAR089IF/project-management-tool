d104d725b58987446313939676f52a575ae4313f
supersem <xsupersemx@gmail.com>
2018-12-15 15:44:24 +0200

    fix

--- app/controllers/account/profiles_controller.rb
+++ app/controllers/account/profiles_controller.rb
@@ -20,6 +20,6 @@ class Account::ProfilesController < ApplicationController
   end
 
   def users_params
-    params.require(:user).permit(:first_name, :last_name, :role, :department, :about, :job_role, :avatar)
+    params.require(:user).permit(:first_name, :last_name, :role, :department, :about, :avatar, :job_role)
   end
 end
