6ba94df9a64dd48328eabdf388e8038f1557a5a8
supersem <xsupersemx@gmail.com>
2018-12-16 13:05:03 +0200

    add ResponsiveContainer

--- app/javascript/bundles/Dashboard/components/TopWorkspacesCard/TopWorkspacesCard.jsx
+++ app/javascript/bundles/Dashboard/components/TopWorkspacesCard/TopWorkspacesCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import _ from "lodash";
 import "./top-workspaces-card.scss";
 
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 class TopWorkspacesCard extends React.Component {
   constructor(props) {
@@ -29,16 +29,18 @@ render() {
 class TopWorkspacesChart extends React.Component {
 	render () {
   	return (
-      <BarChart width={600} height={300} data={this.props.data}
-          margin={{top: 20, right: 30, left: 20, bottom: 5}}>
-        <CartesianGrid strokeDasharray="3 3"/>
-        <XAxis dataKey="name"/>
-        <YAxis/>
-        <Tooltip/>
-        <Legend />
-        <Bar dataKey="completed" stackId="a" fill="#2bb712" />
-        <Bar dataKey="uncompleted" stackId="a" fill="#f44242" />
-      </BarChart>
+      <ResponsiveContainer>
+        <BarChart width={600} height={300} data={this.props.data}
+            margin={{top: 20, right: 30, left: 20, bottom: 5}}>
+          <CartesianGrid strokeDasharray="3 3"/>
+          <XAxis dataKey="name"/>
+          <YAxis/>
+          <Tooltip/>
+          <Legend />
+          <Bar dataKey="completed" stackId="a" fill="#2bb712" />
+          <Bar dataKey="uncompleted" stackId="a" fill="#f44242" />
+        </BarChart>
+      </ResponsiveContainer>
     );
   }
 }
