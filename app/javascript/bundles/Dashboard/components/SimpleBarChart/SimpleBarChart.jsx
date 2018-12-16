f55e43e677fcfadd5915956c83c9e5959de211ee
Natalia <nataliaboyko11081997@gmail.com>
2018-12-16 22:16:44 +0200

    css  styling

--- app/javascript/bundles/Dashboard/components/SimpleBarChart/SimpleBarChart.jsx
+++ app/javascript/bundles/Dashboard/components/SimpleBarChart/SimpleBarChart.jsx
@@ -1,22 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, LabelList, Legend} from 'recharts';
+import {ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, LabelList, Legend} from 'recharts';
 import "./simple-bar-chart.scss";
 
-
-const renderCustomizedLabel = (props) => {
-  const { x, y, value } = props;
-  const radius = 10;
-
-  return (
-    <g>
-      <circle cx={x / 2} cy={y - radius} r={radius} fill="#8884d8" />
-      <text x={x / 2} y={y - radius} fill="#fff" textAnchor="middle" dominantBaseline="middle">
-        {value.split(' ')[0]}
-      </text>
-    </g>
-  );
-};
 
 class SimpleBarChart extends React.Component {
   render () {
@@ -26,16 +12,16 @@ class SimpleBarChart extends React.Component {
 		  );
 		}
   	return (
-    	<BarChart  data={this.props.data}>
-       <CartesianGrid strokeDasharray="10 10"/>
-       <XAxis dataKey="name"/>
-       <YAxis/>
-       <Tooltip/>
-       <Legend />
-       <Bar dataKey="completed" fill="#8884d8" minPointSize={5}>
-          <LabelList dataKey="name" content={renderCustomizedLabel} />
-        </Bar>
-      </BarChart>
+      <ResponsiveContainer>
+      	<BarChart  data={this.props.data}>
+          <CartesianGrid strokeDasharray="3 3"/>
+          <XAxis dataKey="name"/>
+          <YAxis/>
+          <Tooltip/>
+          <Legend />
+          <Bar dataKey="completed" fill="#8884d8" minPointSize={5} />
+        </BarChart>
+      </ResponsiveContainer>
     );
   }
 }
